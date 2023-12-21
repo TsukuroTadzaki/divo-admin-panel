a47848959caf669c27186c010c9d391b66246774
Konstantin Sivenkov <ksivenkov@stone-labs.com>
2023-12-21 13:33:24 +0200

    fix sidebar fixed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -58,7 +58,7 @@ $isEmptyNavbarBanner = Dashboard::isEmptyNavbarBanner();
     <div class="row d-md-flex h-100">
         @yield('aside')
 
-        <div class="col-xxl col-xl-9 col-12">
+        <div class="offset-2 col-10">
             @yield('body')
         </div>
     </div>
