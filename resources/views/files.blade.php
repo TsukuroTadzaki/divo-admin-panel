c6488b240f9f95aba2012afd05eda4449a471c53
Konstantin Sivenkov <ksivenkov@stone-labs.com>
2023-12-25 14:07:28 +0200

    remove elfinder nahooy

--- resources/views/files.blade.php
+++ resources/views/files.blade.php
@@ -1,5 +1,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/elfinder/2.1.64/js/elfinder.min.js" integrity="sha512-2/LGrIY+MyzVq+p9FOnlEH/1ekCJXWMVU9INphJp5GA6lHEWYb3ZETcB5pJDGkUUU04Bc6V9F9dwdj/22fWKqg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script type="text/javascript" charset="utf-8">
         document.addEventListener('DOMContentLoaded', elfinder_handler);
         document.addEventListener('turbo:load', elfinder_handler);
