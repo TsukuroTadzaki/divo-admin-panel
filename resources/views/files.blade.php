c8d40b0ade71c89c9639ee7fcdf1ab1c952f0bb5
Konstantin Sivenkov <ksivenkov@stone-labs.com>
2023-12-25 14:01:12 +0200

    fix jquery filemanager

--- resources/views/files.blade.php
+++ resources/views/files.blade.php
@@ -1,3 +1,4 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script type="text/javascript" charset="utf-8">
         document.addEventListener('DOMContentLoaded', elfinder_handler);
         document.addEventListener('turbo:load', elfinder_handler);
